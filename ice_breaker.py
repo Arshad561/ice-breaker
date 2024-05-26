b8303d60899d81eadee30a5051c019b121dc19d0
Arshad561 <sarshadbasha@gmail.com>
2024-05-25 22:05:46 -0700

    initial commit

---
+++ ice_breaker.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+from langchain.prompts.prompt import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain.chains import LLMChain
+
+from third_parties.linkedin import scrape_linkedin_profile
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    print("Hello LangChain")
+
+    summary_template = """
+    given the Linkedin Information {information} about a person, I want you to create:
+    1. A short summary
+    2. two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    linkedin_data = scrape_linkedin_profile("https://www.linkedin.com/in/arshad-basha-shaik-2085a0129/")
+    res = chain.invoke(input={"information": linkedin_data})
+
+    print(res)
---
+++ third_parties/linkedin.py
@@ -0,0 +1,49 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = True):
+
+    if mock:
+        linkedin_profile_url = "https://gist.githubusercontent.com/Arshad561/f6f4f88ca391f68830bb67e6bf0ac538/raw/2dd03b2245c2796c01c26ce0824e14a87d42f830/arshad-shaik.json"
+        response = requests.get(linkedin_profile_url, timeout=10)
+    else:
+        api_key = os.environ.get("PROXYCURL_API_KEY")
+        headers = {"Authorization": "Bearer " + api_key}
+        api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+        params = {
+            "linkedin_profile_url": linkedin_profile_url,
+        }
+        response = requests.get(
+            api_endpoint,
+            params = params,
+            headers = headers,
+            timeout = 10,
+        )
+
+    data = response.json()
+
+    data = {
+        k: v
+        for k, v in data.items()
+        if v not in ([], "", "", None)
+        and k not in ["people_also_viewed", "certifications"]
+    }
+
+    if data.get("groups"):
+        for group_dict in data.get("groups"):
+            group_dict.pop("profile_pic_url")
+
+    return data
+
+
+if __name__ == "__main__":
+    print(
+        scrape_linkedin_profile(
+            linkedin_profile_url = "https://www.linkedin.com/in/arshad-basha-shaik-2085a0129/",
+            mock = True,
+        )
+    )
